9834bebda72a3c64e9b849a58045e8fe7e51a5b8
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-05-31 16:04:54 +0300

    Improved bookmark board

---
+++ src/components/BookmarkBoard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IoAddOutline } from "react-icons/io5";
+
+export const BookmarkBoard = () => {
+  return (
+    <>
+      <div className="w-full flex flex-3/4 justify-center items-center">
+        <div className="w-full flex justify-center items-center select-none">
+          <h2 className="text-lg text-neutral-500 flex justify-center items-center gap-1">
+            Press
+            <span>
+              <IoAddOutline />
+            </span>
+            to create a new bookmark
+          </h2>
+        </div>
+      </div>
+    </>
+  );}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { Header } from "../components/Header";
+import { BookmarkBoard } from "../components/BookmarkBoard";
 
 export const Home = () => {
   return (
     <>
       <div className="w-full min-h-svh flex flex-col justify-center items-start p-4 gap-4">
         <Header />
-
-        <div className="w-full flex flex-3/4 justify-center items-center">
-          <div className="w-full flex justify-center items-center select-none">
-            <h2 className="text-lg text-neutral-500">
-              Press + to create a new bookmark
-            </h2>
-          </div>
-        </div>
+        <BookmarkBoard />
       </div>
     </>
   );
